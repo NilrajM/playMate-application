b86a2ee9a4b7b8501ce25ec16d5afa061bfbd965
NilrajMayekar <mayekar.n@northeastern.edu>
2023-12-20 00:18:19 -0500

    Controller and service logic for logout and email verification using otp mechanism

--- server/app/controller/userAuth-controller.js
+++ server/app/controller/userAuth-controller.js
@@ -4,9 +4,17 @@ export const registerUser = async(req, res) => {
     try{
         const newUser = {...req.body};
         const user = await userAuthService.saveUser(newUser);
+
+        const otpWithTimeStamp = userAuthService.generateOtpWithTimeStamp();
+        console.log(otpWithTimeStamp);
+        await userAuthService.sendVerificationEmail(
+            user.email,
+            otpWithTimeStamp.otp
+        )
         res.status(201).json({
             success: true,
             user,
+            message: 'User registered. Check your email for verification',
         });
     }
     catch(err){
@@ -26,12 +34,15 @@ export const login = async(req, res) => {
             res.status(401).json({
                 success: false,
                 message: 'Invalide email or password',
-            })
+            });
+            return;
         }
 
         await userAuthService.flagUserLoggedIn(user._id);
 
-        const tokens = userAuthService.generateTokens(user._id);
+        const updatedUser = await userAuthService.getUserById(user._id);
+
+        const tokens = userAuthService.generateTokens(updatedUser._id);
 
         res.header('Authorization', `Bearer ${tokens.accessToken}`);
 
@@ -40,12 +51,52 @@ export const login = async(req, res) => {
         res.status(200).json({
             success: true,
             tokens,
-            user
+            user: updatedUser
         });
     }
     catch(err){
         res.status(500).json({
             success: true,
+            message: err.message,
+        })
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    try{
+        const {email, otp} = req.body;
+        const user = await userAuthService.verifyEmail(email, otp);
+
+        res.status(200).json({
+            success: true, 
+            message: 'Email verified successfully',
+            user,
+        })
+    }
+    catch(err){
+        res.status(400).json({
+            success: false,
+            message: err.message,
+        })
+    }
+}
+
+export const logout = async(req, res) => {
+    try{
+        const userId = req.user._id;
+
+        await userAuthService.flagUserLoggedOut(userId);
+
+        res.clearCookie('refreshToken');
+
+        res.status(200).json({
+            success: true,
+            message: 'User is logged out successfully'
+        });
+    }
+    catch(err){
+        res.status(500).json({
+            success: false, 
             message: err.message,
         })
     }
--- server/app/services/userAuth-service.js
+++ server/app/services/userAuth-service.js
@@ -1,14 +1,41 @@
 import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
+import transporter from '../../emailConfig.js';
+import { totp } from 'otplib';
+
+const otpSecretKey = `${process.env.OTP_SECRET_KEY}`;
+totp.options = {digits: 6, step: 300, crypto: {createHmac: (alg, key) => crypto.createHmac(alg, key)}};
+
 
 export const saveUser = async(newUser) => {
     try{
         const user = new User(newUser);
+
+        const otp = totp.generate(otpSecretKey);
+        const expiryTimeStamp = Date.now() + 3 * 60 * 1000;
+
+        user.verification = {
+            otp, 
+            expiryTimeStamp,
+        }
         await user.save();
         return user;
     }
     catch(err){
         throw new Error(err.message);
+    }
+}
+
+export const getUserById = async(userId) => {
+    try{
+        const user = await User.findById(userId);
+        if(!user){
+            throw new Error('User not found');
+        }
+        return user;
+    }
+    catch(err){
+        throw new Error('Error fetching user by Id from the database')
     }
 }
 
@@ -42,3 +69,69 @@ export const flagUserLoggedIn = async(userId) => {
 export const flagUserLoggedOut = async(userId) => {
     await User.findByIdAndUpdate(userId, {isLoggedIn: false});
 }
+
+// const otpGenerator = generateOtp('1234567890', 6);
+
+export const generateOtpWithTimeStamp = () => {
+    const otp = totp.generate(otpSecretKey);
+    const timeStamp = Date.now();
+    const expirationTime = 3 * 60 * 1000;
+    const expiryTimeStamp = timeStamp + expirationTime;
+
+    return {otp, expiryTimeStamp};
+}
+
+export const sendVerificationEmail = async(email, otp) => {
+    const mailOptions = {
+        from: 'nilrajmayekar@gmail.com',
+        to: email,
+        subject: 'Email verification otp',
+        text: `Your OTP for email verification is: ${otp}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+
+export const verifyEmail = async(email, otp) => {
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new Error('User not found');
+    }
+
+    const otpDetails = await getStoredOtpDetails(user._id);
+    if(!isOtpValid(otp, otpDetails)){
+        throw new Error('Invalid or expired OTP');
+    }
+
+    user.isEmailVerified = true;
+    await user.save();
+    return user;
+}
+
+const isOtpValid = (enteredOtp, otpDetails) => {
+    return (
+        enteredOtp === otpDetails.otp && Date.now() <= otpDetails.expiryTimeStamp
+    );
+};
+
+const getStoredOtpDetails = async(userId) => {
+    try{
+        const user = await User.findById(userId);
+
+        if(!user){
+            throw new Error('User not found');
+        }
+
+        const otpDetails = {
+            otp: user.verification.otp,
+            expiryTimeStamp: user.verification.expiryTimeStamp,
+        };
+
+        return otpDetails;
+    }
+    catch(err){
+        throw new Error('Error fetching OTP details from the database');
+    }
+}
+
