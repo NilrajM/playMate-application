5aaac2512a098a25d77f4d3815ca9ebda8b842ca
NilrajMayekar <mayekar.n@northeastern.edu>
2023-12-20 19:22:33 -0500

    Added service logic for reset, forgot and new access token

--- server/app/services/userAuth-service.js
+++ server/app/services/userAuth-service.js
@@ -1,5 +1,6 @@
 import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import transporter from '../../emailConfig.js';
 import { totp } from 'otplib';
 
@@ -132,6 +133,98 @@ const getStoredOtpDetails = async(userId) => {
     }
     catch(err){
         throw new Error('Error fetching OTP details from the database');
+    }
+}
+
+export const findUserByEmail = async(email) => {
+    return await User.findOne({email});
+}
+
+export const generateResetToken = (userId) => {
+    const resetToken = jwt.sign({userId: userId}, process.env.RESET_TOKEN_SECRET, {expiresIn: '15m'});
+    return resetToken;
+}
+
+export const updateResetToken = async(userId, resetToken, resetExpireDate) => {
+    await User.findByIdAndUpdate(userId, {
+        resetPasswordToken: resetToken,
+        resetPasswordExpireDt: resetExpireDate
+    });
+}
+
+export const sendResetPasswordLink = async(email, resetToken) => {
+    try{
+        const resetLink = `${resetToken}`;
+        const mail = {
+            from: 'nilrajmayekar@gmail.com',
+            to: email,
+            subject: 'Reset password request',
+            html: `<p>You have requested to reset your password. Please click on the following link: </p>
+                    <a href="#" target="_blank">${resetLink}</a>`,
+        };
+
+        await transporter.sendMail(mail);
+        return true;
+    }
+    catch(err){
+        throw new Error(err.message);
+    }
+}
+
+export const forgotPassword = async(email) => {
+    try{
+        const user = await findUserByEmail(email);
+
+        if(!user){
+            return {success: false};
+        }
+
+        const resetToken = generateResetToken(user._id);
+        const resetExpireDate = new Date();
+        resetExpireDate.setMinutes(resetExpireDate.getMinutes() + 15);
+
+        await updateResetToken(user._id, resetToken, resetExpireDate);
+
+        await sendResetPasswordLink(email, resetToken);
+        
+        return {success: true};
+    }
+    catch(err){
+        throw new Error(err.message);
+    }
+}
+
+export const verifyResetToken = (resetToken) => {
+    try{
+        const decoded = jwt.verify(resetToken, process.env.RESET_TOKEN_SECRET);
+        return decoded.userId;
+    }
+    catch(err){
+        throw new Error('Invalid reset token');
+    }
+}
+
+export const updatePassword = async(userId, newPassword) => {
+    const salt = await bcrypt.genSalt(12);
+    const hashedPass = await bcrypt.hash(newPassword, salt);
+
+    await User.findByIdAndUpdate(userId, {
+        password: hashedPass,
+        resetPasswordToken: null, 
+        resetPasswordExpireDt: null,
+    });
+}
+
+export const getNewAccessToken = (refreshToken) => {
+    try{
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+        const accessToken = jwt.sign({userId: decoded.userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'});
+
+        return accessToken;
+    }
+    catch(err){
+        throw new Error('Invalid refresh token');
     }
 }
 
