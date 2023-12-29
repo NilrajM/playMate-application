6c246d46407d52d2117c72969c9b43874659ac98
NilrajMayekar <mayekar.n@northeastern.edu>
2023-12-29 15:39:21 -0500

    Update the reset password page route

--- client/playmate-app/src/App.tsx
+++ client/playmate-app/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <Route path='/signin' element={<SignIn/>}/>
           <Route path='/signup' element={<SignUp/>}/>
           <Route path='/forgotpassword' element={<ForgotPassword/>}/>
-          <Route path='/resetpassword' element={<ResetPassword/>}/>
+          <Route path='/resetpassword/:resetToken' element={<ResetPassword/>}/>
         </Routes>
     </Router>
   );
